4e5df1addff24b623db9e56526588644e575292c
Robins, Kevin <kevin.robins@intel.com>
2019-09-26 11:40:57 -0700

    Lunchr now recognizes taco tuesday! Salsa's is always selected for, and only for, tuesday lunches!

--- lunchr.py
+++ lunchr.py
@@ -1,9 +1,17 @@
 #!/p/nvm/env/commontools/bin/python3
 # -*- coding: utf-8 -*-
 import random as rand
+import datetime
+import sys
 
-places = ["Cafe", "Pho ABC", "Salsas", "BJs", "Johnny Rockets", "Fire Rock", "Buckhorn", "Thai Paradise", "Land Ocean", "River Thai", "Pho M", "Mongolian BBQ", "Pieology", "Islands"]
+date = datetime.datetime.today()
+day = date.strftime('%A')
 
+places = ["Cafe", "Pho ABC", "BJs", "Johnny Rockets", "Fire Rock", "Buckhorn", "Thai Paradise", "Land Ocean", "River Thai", "Pho M", "Mongolian BBQ", "Pieology", "Islands"]
+
+if(day == "Tuesday"):
+	print("Taco Tuesday @ Salsa's!")
+	sys.exit(0)
 
 choice = places[rand.randint(0, len(places)-1)]
 print(choice)
